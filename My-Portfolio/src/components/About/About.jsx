b0dd0d1efdcdce43dc7a0b64a5764dc24212fb7d
Sean Clayton <zweihander555@gmail.com>
2025-01-07 11:35:25 -0800

    Updated Contact, About, and Experience sections. Added images for About section.

--- My-Portfolio/src/components/About/About.jsx
+++ My-Portfolio/src/components/About/About.jsx
@@ -9,13 +9,13 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
-          alt="Me sitting with a laptop"
-          className={styles.aboutImage}
+          className = {styles.aboutMe}
+          src={getImageUrl("about/capability.png")}
+          alt="Random image"
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img className = {styles.development} src={getImageUrl("about/continuous.png")} alt="Server icon" />
+            <img className = {styles.development} src={getImageUrl("about/cycle.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
               <h3>Exposure to Software Development Lifecycle</h3>
               <p>
@@ -24,16 +24,16 @@ export const About = () => {
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <img className = {styles.database} src={getImageUrl("about/database.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Proficient in Back-End Development</h3>
               <p>
-              Skilled in creating robust backend architectures using Node.js, Express, and RESTful APIs, with a focus on scalability and database optimization.
+              Skilled in creating robust backend architectures using Node.js, Express, and low-level programming languages, with a focus on scalability and modular design.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <img className = {styles.algorithm} src={getImageUrl("about/numbers.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
               <h3>Hands-On Experience with Advanced Algorithms</h3>
               <p>
